56a5b8c1c407c98f191977aa5a6ea9e3e3b80da5
XMKT <50673737+XMKT@users.noreply.github.com>
2023-01-31 02:17:06 +0300

    Add files via upload

--- ArrayCode.py
+++ ArrayCode.py
@@ -1,14 +1,24 @@
-# достает значение используя код
-def GetValueWithCode(array,code): # переделать
+# достает значение используя код [0,1,2] или "012"
+def GetValueWithCode(array,code): 
 	try:
 		value = array
 		for i in code:
-			value = value[i]
+			value = value[int(i)]
 
 		return value
 	except:
 		print("Значение кода " + str(code) + " - не существует")
 
+# меняет местами ключи и значения
+def InvertDict(CodeDict):
+	key = list(CodeDict.keys())
+	val = list(CodeDict.values())
+	NewDict = {}
+	for i in key:
+		NewDict[CodeDict[i]]=i
+	return NewDict
+
+# создает словарь из массива( используеться для расчленения детей ;) )
 def index(array):
 	result = {}
 	num = 0
@@ -17,6 +27,8 @@ def index(array):
 		num = num + 1
 	return result
 
+# функция которая ищет детей уровня(ответвления)
+# принимает и возвращает словарь
 def Branches(MyDict):
 	for i in MyDict:
 		val = MyDict[i]
@@ -25,6 +37,8 @@ def Branches(MyDict):
 			MyDict[i]=NewDict
 	return MyDict
 
+# функция помогает перебраться на следующий уровень, изменяя ключи, добавляя в них ссылку на родителя
+# принимает и возвращает словарь
 def GoOnLevel(MyDict):
 	NewDict = {}
 	for i in MyDict:
@@ -37,7 +51,7 @@ def GoOnLevel(MyDict):
 				NewDict[i+n] = value[n]
 	return NewDict
 
-# команда создающая код
+# функция создающая код
 # возвращает словарь
 def CreateCode(array):
 	MainDict = index(array)
@@ -48,12 +62,4 @@ def CreateCode(array):
 		if a == MainDict:
 			bebra = False
 		MainDict = a
-
-CreateCode(a)
-
-
-
-
-
-
-
+	return MainDict
